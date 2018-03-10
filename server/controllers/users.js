3be234b376d2f1f915f5a1286b93d55bb3dc5e99
Anthony Poon <marquis.andras@gmail.com>
2018-03-10 21:26:49 +1100

    Add the ability to get the ShareHoldings for a user

---
+++ server/controllers/users.js
@@ -0,0 +1,8 @@
+// @flow
+import '../../lib/users'
+
+function read (req, res) {
+}
+
+function update (req, res) {
+}
--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
+// @flow
 import express from 'express'
+import * as users from './controllers/users'
 
 const app = express()
+
+app.get('/api/users', users.read)
 
 app.listen(3000)
